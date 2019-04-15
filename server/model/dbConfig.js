ee5db733ae0f557e2e332110036ed6a43ece863d
Damilola Ipadeola <damilolaipadeola23@gmail.com>
2019-04-15 18:51:46 +0100

    setting up database

---
+++ server/model/dbConfig.js
@@ -0,0 +1,14 @@
+import {Pool} from "pg";
+import dotenv from "dotenv";
+import config from "../config";
+dotenv.config();
+
+const environment = proces.env.NODE_ENV
+let pool;
+
+if (environment === "development") {
+    pool = new Pool(config.development);
+}
+
+
+export default pool;
